a55245ed877b665714d6d1b2fb0f9bb419e911b1
huanken <koon2105@gmail.com>
2021-11-17 21:17:03 +0700

    update

---
+++ src/server.js
@@ -0,0 +1,24 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+// app.use(express.static(__dirname + '/dist/'));
+// app.use('/src/assets', express.static(__dirname + '/src/assets/'));
+
+
+// app.listen(process.env.PORT || 8080);
+
+//server.js
+const express = require('express');
+var path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use('src/assets', express.static(path.join(__dirname + 'src/assets/')));
+app.use('src/animations', express.static(path.join(__dirname + 'src/animations/')));
+
+app.set('port', process.env.PORT || 8080);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});
