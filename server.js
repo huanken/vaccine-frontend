63aa547db13964164128ac3c6607a639c309e894
huanken <koon2105@gmail.com>
2021-11-18 10:13:10 +0700

    revert

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ const port = process.env.PORT || 8080; //Line 3
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, '/src/assets')));
-// app.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function (req, res) {
     const index = path.join(__dirname, 'build', 'index.html');
