fa06fe17e44c6b41d77b3e246ef8489e22ed28b4
huanken <koon2105@gmail.com>
2021-11-18 08:57:10 +0700

    revert

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const port = process.env.PORT || 8080; //Line 3
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // This displays message that the server running and listening to specified port
