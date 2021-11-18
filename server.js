5f675213f16c2d5b491b4c4511034650c2b3a94b
huanken <koon2105@gmail.com>
2021-11-18 08:38:34 +0700

    revert

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-const express = require('express'); //Line 1
-const app = express(); //Line 2
+
+
 const port = process.env.PORT || 8080; //Line 3
-const path = require('path'); //Line 1
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(__dirname + '/public'));
-    app.get('/', function(req, res) {
-
-        // ejs render automatically looks in the views folder
-        res.render('index');
-    });
-}
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
